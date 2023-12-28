6d4dddd2e2ce56942c4afeaeb667c8ebcf8553bf
BlindException <robertprock31@gmail.com>
2023-12-28 12:23:06 -0500

    updateupdate

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -14,36 +14,4 @@ class LoginController extends Controller
     {
         return Socialite::driver('yahoo')->redirect();
     }
-
-    public function handleYahooCallback()
-    {
-        $user = Socialite::driver('yahoo')->user();
-        // Handle the user data returned by Yahoo Fantasy API
-        $code = request()->input('code');
-        dd($code);
-        $state = request()->input('state');
-        $client = new Client();
-        $response = $client->request('POST', 'https://api.login.yahoo.com/oauth2/get_token', [
-            'form_params' => [
-                'client_id' => env('YAHOO_CLIENT_ID'),
-                'client_secret' => env('YAHOO_CLIENT_SECRET'),
-                'redirect_uri' => env('YAHOO_REDIRECT_URI'),
-                'code' => $code,
-                'grant_type' => 'authorization_code'
-            ]
-        ]);
-        $accessToken = json_decode($response->getBody())->access_token;
-        $client = new Client([
-            'headers' => [
-                'Authorization' => 'Bearer ' . $accessToken,
-                'Accept' => 'application/json'
-            ]
-        ]);
-        $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games/leagues');
-
-        $leagues = json_decode($response->getBody())->fantasy_content->users->user->games->game->leagues;
-        // Process the leagues data here
-        return redirect()->route('pkeepers.index', ['leagues' => $leagues,
-        ]);
-    }
 }
--- app/Http/Controllers/KeeperController.php
+++ app/Http/Controllers/KeeperController.php
@@ -11,13 +11,11 @@ class KeeperController extends Controller
 
     public function index()
     {
-        if (request()) {
+        if (request('code')) {
             $user = Socialite::driver('yahoo')->user();
-            $code = request()->input('code');
-            $state = request()->input('state');
             $accessToken = $user->accessTokenResponseBody['access_token'];
             $client = new Client();
-            $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users/' . $user->id . '/leagues;game_keys=nfl', [
+            $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users/' . $user->id . '/profile', [
                 'headers' => [
                     'Authorization' => 'Bearer ' . $accessToken,
                     'Accept' => 'application/json',
@@ -25,10 +23,22 @@ class KeeperController extends Controller
             ]);
             $body = $response->getBody();
             $data = json_decode($body, true);
-            // Now you can access the leagues data
-            $leagues = $data['fantasy_content']['leagues'];
-            // Do something with the leagues data
-            return view('pkeepers.index', ['leagues' => $leagues]);
+            // Now you can access the user's profile data
+            $profile = $data['fantasy_content']['users'];
+            // Do something with the profile data
+            dd($profile);
+            /*            $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users/' . $user->id . '/leagues;game_keys=nfl', [
+                            'headers' => [
+                                'Authorization' => 'Bearer ' . $accessToken,
+                                'Accept' => 'application/json',
+                            ],
+                        ]);
+                        $body = $response->getBody();
+                        $data = json_decode($body, true);
+                        // Now you can access the leagues data
+                        $leagues = $data['fantasy_content']['leagues'];
+                        // Do something with the leagues data
+                        return view('pkeepers.index', ['leagues' => $leagues]);*/
         } else {
             abort('No Leagues');
         }
