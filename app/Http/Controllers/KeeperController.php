ab266bd2a128a082b61eca11f6188049a96b9061
BlindException <robertprock31@gmail.com>
2023-12-29 00:45:45 -0500

    update

--- app/Http/Controllers/KeeperController.php
+++ app/Http/Controllers/KeeperController.php
@@ -14,34 +14,18 @@ class KeeperController extends Controller
         if (request()) {
             $user = Socialite::driver('yahoo')->user();
             $accessToken = $user->accessTokenResponseBody['access_token'];
-            $client = new Client();
-            $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/leagues', [
+            $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users/' . $user->id . '/leagues;game_keys=nfl', [
                 'headers' => [
                     'Authorization' => 'Bearer ' . $accessToken,
                     'Accept' => 'application/json',
                 ],
             ]);
-            $status = $response->getStatusCode();
-            $error = json_decode($response->getBody(), true)['error'];
-            dd($error . $status);
             $body = $response->getBody();
             $data = json_decode($body, true);
-            // Now you can access the user's profile data
-            //$profile = $data['fantasy_content']['users'];
-            // Do something with the profile data
-            dd($data);
-            /*            $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users/' . $user->id . '/leagues;game_keys=nfl', [
-                            'headers' => [
-                                'Authorization' => 'Bearer ' . $accessToken,
-                                'Accept' => 'application/json',
-                            ],
-                        ]);
-                        $body = $response->getBody();
-                        $data = json_decode($body, true);
-                        // Now you can access the leagues data
-                        $leagues = $data['fantasy_content']['leagues'];
-                        // Do something with the leagues data
-                        return view('pkeepers.index', ['leagues' => $leagues]);*/
+            // Now you can access the leagues data
+            $leagues = $data['fantasy_content']['leagues'];
+            // Do something with the leagues data
+            return view('pkeepers.index', ['leagues' => $leagues]);
         } else {
             abort('No Leagues');
         }
