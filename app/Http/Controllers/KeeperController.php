b8a25af03b82255c45386f3fdd565c5d3addf507
BlindException <robertprock31@gmail.com>
2023-12-29 18:37:03 -0500

    update

--- app/Http/Controllers/KeeperController.php
+++ app/Http/Controllers/KeeperController.php
@@ -16,7 +16,7 @@ class KeeperController extends Controller
             $accessToken = $user->accessTokenResponseBody['access_token'];
             $client = new Client();
             try {
-                $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/;use_login=1/games/leagues', [
+                $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_key=nfl/leagues', [
                     'headers' => [
                         'Authorization' => 'Bearer ' . $accessToken,
                         'Accept' => 'application/json',
