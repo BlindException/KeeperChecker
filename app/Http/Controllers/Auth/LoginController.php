f7ed77f0a793a456566e936c1d9af991dfdffa7a
BlindException <robertprock31@gmail.com>
2023-12-29 11:40:21 -0500

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,8 +35,7 @@ class LoginController extends Controller
             'fspt-w-lm-settings',
         ];
         return Socialite::driver('yahoo')
-            //->scopes($scopes)
-            ->with(['grant_type' => 'authorization_code'])
+            ->with(['grant_type' => 'authorization_code', 'scopes' => $scopes])
             ->redirect();
     }
 }
