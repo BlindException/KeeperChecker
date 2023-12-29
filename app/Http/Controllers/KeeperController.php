e7dfb9937d78896c42f2ff161ff123fa58935f02
BlindException <robertprock31@gmail.com>
2023-12-29 00:50:17 -0500

    update

--- app/Http/Controllers/KeeperController.php
+++ app/Http/Controllers/KeeperController.php
@@ -14,6 +14,7 @@ class KeeperController extends Controller
         if (request()) {
             $user = Socialite::driver('yahoo')->user();
             $accessToken = $user->accessTokenResponseBody['access_token'];
+            $client = new Client();
             $response = $client->request('GET', 'https://fantasysports.yahooapis.com/fantasy/v2/users/' . $user->id . '/leagues;game_keys=nfl', [
                 'headers' => [
                     'Authorization' => 'Bearer ' . $accessToken,
